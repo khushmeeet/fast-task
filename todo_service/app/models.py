b69226dd2ae7ab75776e470b7c978bc4bec24ce8
Khushmeet Singh <khushmeetsingh1996@gmail.com>
2023-02-27 18:19:00 -0500

    Create github action for docker build & push

--- todo_service/app/models.py
+++ todo_service/app/models.py
@@ -9,7 +9,7 @@ from db import StatusEnum
 
 
 class TodosModel(BaseModel):
-    id: str = Field(..., alias="_id")
+    id: Optional[str] = Field(..., alias="_id")
     title: str
     desc: Optional[str]
     tags: Optional[List[str]]
